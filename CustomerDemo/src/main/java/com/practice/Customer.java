ca8c6e6739b97dce13f2b993725d923a9e130ab2
shubhgar <shubhgar@LIN51017713.corp.capgemini.com>
2021-11-20 13:45:49 +0530

    question 9

---
+++ CustomerDemo/src/main/java/com/practice/Customer.java
@@ -0,0 +1,39 @@
+package com.practice;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Customer {
+	@Id
+	private String name;
+	private String address;
+	private int number;
+	public Customer(String name, String address, int number) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	
+
+}
