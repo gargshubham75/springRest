3d3b00e63294c3da9237a223e10a3be62396e21b
gargshubham75 <gargshubham75@gmail.com>
2021-11-20 13:40:54 +0530

    question 4

---
+++ assignmentRest-4/src/main/java/com/practice/AssignmentRest4Application.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentRest4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentRest4Application.class, args);
+	}
+
+}
